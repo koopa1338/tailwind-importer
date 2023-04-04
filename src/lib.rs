ac67f6ba50598938e6f48d6db6cfb349bec3107f
koopa1338 <sinner1991@gmail.com>
2023-04-04 13:54:30 +0200

    add tailwind config

--- src/lib.rs
+++ src/lib.rs
@@ -16,18 +16,43 @@ pub fn build_library() {
 }
 
 /// Call this function in the `build.rs` of the main application
-pub fn build_frontend() {
+pub fn build_frontend(output: impl AsRef<Path>, input: impl AsRef<Path>, minify: bool) {
     let target = std::env::var_os("OUT_DIR").unwrap();
     let target_dir = Path::new(&target).parent().unwrap().parent().unwrap();
-    std::process::Command::new("tailwindcss")
-        .arg("--content")
-        .arg(format!(
-            "{}/**/*.rs,./src/**/*.{{html,rs}},./index.html",
-            target_dir.display(),
-        ))
-        .arg("-o")
-        .arg("./tailwind.css")
-        .arg("--minify")
+    let mut content = vec![format!("{}/**/*.rs", target_dir.display())];
+
+    // Merge additional content options if a tailwind config file is present
+    let root = std::env::var_os("CARGO_MANIFEST_DIR").unwrap();
+    let tailwind_config = Path::new(&root).join("tailwind.config.js");
+    if tailwind_config.exists() {
+        let config = std::fs::read_to_string(tailwind_config).unwrap();
+        let content_opt = config.find("content:").unwrap();
+        let start = config[content_opt..].find('[').unwrap() + content_opt + 1;
+        let end = config[content_opt..].find(']').unwrap() + content_opt;
+        let config_content = config[start..end]
+            .split(',')
+            .map(|s| {
+                let mut option = String::from(s);
+                option.retain(|c| !c.is_whitespace());
+                option
+            })
+            .collect::<Vec<_>>();
+        content.extend_from_slice(&config_content);
+        content.dedup();
+    }
+
+    let conten_arg = content.join(",");
+    let out = &output.as_ref().display().to_string();
+    let inp = &input.as_ref().display().to_string();
+    let mut args = vec!["--content", &conten_arg, "-o", &out];
+    if minify {
+        args.push("--minify");
+    }
+    std::process::Command::new("npx")
+        .arg("tailwindcss")
+        .arg("-i")
+        .arg(&inp)
+        .args(&args)
         .output()
         .expect("failed to execute process");
 
